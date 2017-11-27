8b5d5cb2dd35004da491f9777329f73953120228
khardi <kharchilindanila@gmail.com>
2017-11-27 21:28:03 +0300

    tests

--- tests/test_tablo.py
+++ tests/test_tablo.py
@@ -1,95 +1,37 @@
 """
 
-Таблица
-=======
-
-Создание и доступ
------------------
+Таблица с разделителями
+=======================
 
 Подключаем таблицу
 
-    >>> from src.base import BaseTablo
+    >>> from src import Tablo
 
-Создание таблицы (с указанием заголовков)
+    >>> t = Tablo('X  Y  Z  A  B'.split())
+    >>> t.append_row('@ 4   2.1 False Нет'.split())
+    >>> t.append_row('$ 5.2 8   True  Да '.split())
 
-    >>> headers = 'X Y Z A B'.split()
-    >>> tablo = BaseTablo(headers)
+Печать таблицы (Авто-выравнивание ширины колонок)
 
-Добавление строки в таблицу
+    # >>> [x for x in dir(t[0]) if not x.startswith('_')]
 
-    >>> row = '@', 4, 2.1, False, 'Нет'
-    >>> tablo.append_row(row)
+    >>> t.print()
+    | X | Y   | Z   | A     | B   |
+    | @ | 4   | 2.1 | False | Нет |
+    | $ | 5.2 | 8   | True  | Да  |
 
-Доступ к строке по индексу, доступ к ячейке по имени столбца
+Ручное выравнивание + автовыравнивание колонки
 
-    >>> row = tablo[0]
-    >>> row.X, row.Y, row.Z, row.A, row.B
-    ('@', 4, 2.1, False, 'Нет')
+    >>> t.X.margin = 10
+    >>> t.X.centred()
+    >>> t.B.margin = 15
+    >>> t.B.centred()
 
-Доступ к строке по индексу, доступ к ячейке по индексу столбца
+    >>> t.A.not_spaced()
 
-    >>> row = tablo[0]
-    >>> row[0], row[1], row[2], row[3], row[4]
-    ('@', 4, 2.1, False, 'Нет')
-
-Добавление строки в таблицу
-
-    >>> row = '&', 22, 5.03, True, 'Да'
-    >>> tablo.append_row(row)
-
-Доступ к столбцу по индексу строки
-
-    >>> column_X = tablo.X
-    >>> column_X[0], column_X[1]
-    ('@', '&')
-
-Перебор строк и доступ к данным строки
-
-    >>> for row in tablo:
-    ...     row.data
-    ['@', 4, 2.1, False, 'Нет']
-    ['&', 22, 5.03, True, 'Да']
-
-Перебор строк и столбцов
-
-    >>> for row in tablo:
-    ...     [col for col in row]
-    ['@', 4, 2.1, False, 'Нет']
-    ['&', 22, 5.03, True, 'Да']
-
-
-Ошибки
-======
-
-Добавление сложного типа в ячейку
-
-    >>> row = ('@', 4, 2.1, False, ['invalid', 'data'])
-    >>> tablo.append_row(row)
-    Traceback (most recent call last):
-        ...
-    ValueError: '['invalid', 'data']' Invalid Primitive type
-
-Неполная строка
-
-    >>> row = ('45', 18.2)
-    >>> tablo.append_row(row)
-    >>> row = tablo[2]
-    >>> row.X, row.Y, row.Z, row.A, row.B
-    ('45', 18.2, None, None, None)
-
-Доступ к несуществующей строке
-
-    >>> column_X[10]
-    Traceback (most recent call last):
-      ...
-    AttributeError: '10' Invalid row index
-
-Доступ к несуществующему атрибуту по имени столбца
-
-    >>> row = tablo[0]
-    >>> row.G
-    Traceback (most recent call last):
-      ...
-    AttributeError: 'G' Invalid column name
+    >>> t.print()
+    |     X      | Y   | Z   |A    |        B        |
+    |     @      | 4   | 2.1 |False|       Нет       |
+    |     $      | 5.2 | 8   |True |       Да        |
 
 """
